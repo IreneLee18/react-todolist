74675eff9f0ebc74d1206922ed0fe7993f8a3ea5
IreneLee18 <cc86418520@gmail.com>
2022-08-22 22:20:31 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,201 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+// component
+function AddContent({ newTodo, setNewTodo, addTodo }) {
+  return (
+    <div className="addContent">
+      <input
+        type="text"
+        placeholder="請新增代辦事項"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value.trim())}
+        onKeyPress={(e) => {
+          if (e.key === "Enter") addTodo();
+        }}
+      />
+      <button onClick={addTodo}></button>
+    </div>
+  );
+}
+function Tab({ tab, currentTab, setCurrentTab }) {
+  return (
+    <>
+      {tab.map((item) => (
+        <li
+          key={item.id}
+          className={item.id === currentTab ? "currentTab" : ""}
+          id={item.id}
+          onClick={() => setCurrentTab(item.id)}
+        >
+          {item.type}
+        </li>
+      ))}
+    </>
+  );
+}
+function TodoItem({ currentTodoItem, done, clean }) {
+  return (
+    <>
+      {currentTodoItem.map((item) => (
+        <li key={item.id}>
+          <label htmlFor={item.id}>
+            <input
+              type="checkbox"
+              id={item.id}
+              checked={item.isDone}
+              onChange={done}
+            />
+            <div className={item.isDone ? "check" : ""}></div>
+            <p className={item.isDone ? "done" : ""}>{item.todo}</p>
+          </label>
+          <button id={item.id} onClick={clean}>
+            ✕
+          </button>
+        </li>
+      ))}
+    </>
+  );
+}
 
 function App() {
+  // hooks
+  const [newTodo, setNewTodo] = useState("");
+  const tab = [
+    {
+      id: "all",
+      type: "全部",
+    },
+    {
+      id: "undone",
+      type: "待完成",
+    },
+    {
+      id: "done",
+      type: "已完成",
+    },
+  ];
+  const [currentTab, setCurrentTab] = useState("all");
+  const [todoItem, setTodoItem] = useState([
+    {
+      id: Math.random(),
+      todo: "起床",
+      isDone: true,
+    },
+    {
+      id: Math.random(),
+      todo: "吃飯",
+      isDone: false,
+    },
+    {
+      id: Math.random(),
+      todo: "洗澡",
+      isDone: false,
+    },
+    {
+      id: Math.random(),
+      todo: "睡覺",
+      done: false,
+    },
+  ]);
+  const [currentTodoItem, setCurrentTodoItem] = useState(todoItem);
+  const undone = todoItem.filter((item) => !item.isDone);
+  const undoneLeg = undone.length;
+  // function
+  const addTodo = () => {
+    if (newTodo !== "") {
+      setTodoItem((state) => [
+        ...state,
+        {
+          id: Math.random(),
+          todo: newTodo,
+          isDone: false,
+        },
+      ]);
+      setCurrentTodoItem(todoItem);
+      setNewTodo("");
+    }
+  };
+  // 1.監聽currentTab，當切換tab就執行 2.監聽todoItem，當切換check就執行
+  useEffect(() => {
+    switch (currentTab) {
+      case "all":
+        setCurrentTodoItem(todoItem);
+        break;
+      case "undone":
+        setCurrentTodoItem(todoItem.filter((item) => !item.isDone));
+        break;
+      case "done":
+        setCurrentTodoItem(todoItem.filter((item) => item.isDone));
+        break;
+      default:
+        break;
+    }
+  }, [currentTab, todoItem]);
+  const done = (e) => {
+    // 要取得的事checked的值，才能將checked值放到done上（原因：checked是負責切換checkbox的）
+    const { id, checked } = e.target;
+    return setTodoItem(
+      todoItem.map((item) =>
+        item.id === Number(id) ? { ...item, isDone: checked } : item
+      )
+    );
+  };
+  const clean = (e) => {
+    setTodoItem(todoItem.filter((item) => item.id !== Number(e.target.id)));
+  };
+  const cleanAll = () => {
+    setTodoItem(todoItem.filter((item) => !item.isDone));
+    setCurrentTab("all");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <img src="https://upload.cc/i1/2022/03/23/8vTzYG.png" alt="logo" />
+        <ul>
+          <li>
+            <p>我的代辦</p>
+          </li>
+          <li>
+            <p>登出</p>
+          </li>
+        </ul>
       </header>
-    </div>
+      <main>
+        <AddContent
+          newTodo={newTodo}
+          setNewTodo={setNewTodo}
+          addTodo={addTodo}
+        />
+        {todoItem.length > 0 ? (
+          <div className="content">
+            <ul className="tab">
+              <Tab
+                tab={tab}
+                setCurrentTab={setCurrentTab}
+                currentTab={currentTab}
+              />
+            </ul>
+            <ul className="todolist">
+              <TodoItem
+                currentTodoItem={currentTodoItem}
+                done={done}
+                clean={clean}
+              />
+            </ul>
+            <div className="bottom">
+              <span>{undoneLeg} 個待完成項目</span>
+              <button onClick={cleanAll}>清除已完成項目</button>
+            </div>
+          </div>
+        ) : (
+          <div className="empty">
+            <div>
+              <h2>目前無待辦事項 !</h2>
+            </div>
+          </div>
+        )}
+      </main>
+    </>
   );
 }
 
