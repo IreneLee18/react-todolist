7118f890cd48996a7289a4bdc7b2a48da5350c3d
IreneLee18 <cc86418520@gmail.com>
2022-08-23 17:24:20 +0800

    調整資料夾路徑

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import './_reset.scss';
-import './_style.scss';
+import './style/_reset.scss';
+import './style/_style.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
